d170b6eda2b8b39adc08d500982bae4885ff20f2
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-06 09:50:00 +0530

    adding Helloworld program uc1

--- src/com/praveena/Demo/Helloworld.java
+++ src/com/praveena/Demo/Helloworld.java
@@ -3,5 +3,6 @@ package com.praveena.Demo;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Helloworld");
+        System.out.println("uc1 completed");
     }
 }
